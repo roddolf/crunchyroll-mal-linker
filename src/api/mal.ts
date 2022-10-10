99ff48fac6ca719301817d38e19e02f470468d69
= <=>
2022-10-09 18:10:11 -0700

    Add check for related animes

--- src/api/mal.ts
+++ src/api/mal.ts
@@ -1,9 +1,11 @@
 import { request } from "./common";
 
+export interface DataNode<T> {
+  node: T;
+}
+
 export interface Search<T> {
-  data: Array<{
-    node: T;
-  }>;
+  data: Array<DataNode<T>>;
   pagination: {
     previous?: string;
     next?: string;
@@ -18,33 +20,69 @@ export interface AnimeItem {
     ja?: string;
     synonyms?: Array<string>;
   };
+  related_anime?: Array<RelatedAnime>;
+}
+
+export type SearchAnime = Search<Pick<AnimeItem, 'id' | 'title' | 'alternative_titles'>>;
+
+export interface RelatedAnime extends DataNode<AnimeItem> {
+  relation_type?: string;
 }
 
 const api_url = `https://api.myanimelist.net/v2`;
 const mal_url = `https://myanimelist.net`;
 
+const requestMal = <T>(method: 'GET' | 'HEAD' | 'POST', url: string) => request<T>(method, `${api_url}/${url}`, {
+  'X-MAL-CLIENT-ID': '96884d8bfed54d526cc619941f4398af',
+});
+
 export const searchAnime = async (name: string): Promise<string | undefined> => {
-  const url = `${api_url}/anime?q=${encodeURI(name)}&limit=25&fields=alternative_titles`;
+  const url = `anime?q=${encodeURI(name)}&limit=25&fields=alternative_titles`;
+  const result = await requestMal<SearchAnime>('GET', url);
+  if (!result?.data?.length) return;
 
-  const { data: animes } = await request<Search<AnimeItem>>("GET", url, {
-    'X-MAL-CLIENT-ID': '96884d8bfed54d526cc619941f4398af',
-  });
-  if (!animes) return;
+  let foundAnimes = await findAnimeByName(result.data, name);
 
-  // Find exact match
-  let anime = animes.find(anime =>
-    anime.node.title === name ||
-    anime.node.alternative_titles?.en === name
-  );
-
-  // Use first one if no exact match found
-  if (!anime) {
-    anime = animes[0];
+  // If no exact match found, check if first one has a related match
+  if (!foundAnimes?.length) {
+    const mostRelatedAnime = await getAnime(result.data[0].node.id);
+    foundAnimes = await findAnimeByName(mostRelatedAnime?.related_anime, name);
   }
 
-  // Return if nothing found
-  if (!anime) return;
+  // Select better match
+  const anime = foundAnimes?.length
+    ? foundAnimes[0]
+    : result.data[0];
 
   // Build url
   return `${mal_url}/anime/${anime.node.id}/${anime.node.title}`;
 };
+
+export const getAnime = async (id: number): Promise<AnimeItem | undefined> => {
+  const url = `anime/${id}?fields=alternative_titles,related_anime{alternative_titles}`;
+  const result = await requestMal<AnimeItem>('GET', url);
+  if (!result) return;
+
+  return result;
+}
+
+const findAnimeByName = async (animes: DataNode<AnimeItem>[] | undefined, name: string): Promise<DataNode<AnimeItem>[] | undefined> => {
+  // Return if nothing provided
+  if (!animes?.length) return [];
+
+  // Find options with related name
+  let foundAnimes = animes.filter(anime =>
+    anime.node.title === name ||
+    anime.node.alternative_titles?.synonyms?.find(x => x === name)
+  );
+  if (foundAnimes.length === 1) return foundAnimes;
+
+  // Look for exact name if more than one
+  foundAnimes = animes.filter(anime =>
+    anime.node.alternative_titles?.en === name
+  );
+  if (foundAnimes.length === 1) return foundAnimes;
+
+  // No match found
+  return undefined;
+}
