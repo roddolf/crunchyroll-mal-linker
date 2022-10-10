bc17b0721ec2aeac87b2b54b24725e0562764124
= <=>
2022-10-09 23:23:52 -0700

    Add anime URL caching to Jikan

--- src/api/jikan.ts
+++ src/api/jikan.ts
@@ -1,3 +1,4 @@
+import { UrlCache } from "./caching";
 import { request } from "./common";
 
 export interface Search<T> {
@@ -23,9 +24,18 @@ export interface AnimeItem {
   }>;
 }
 
+const cache = new UrlCache({
+  maxSize: 5000,
+  ttl: 1000 * 60 * 5,
+});
+
 const api_url = `https://api.jikan.moe/v4`;
 
 export const searchAnime = async (name: string): Promise<string | undefined> => {
+  // Check cache
+  const cachedURL = cache.get(name);
+  if (cachedURL) return cachedURL;
+
   const url = `${api_url}/anime?q=${encodeURI(name)}&limit=25`;
 
   const { data: animes } = await request<Search<AnimeItem>>("GET", url);
@@ -33,5 +43,11 @@ export const searchAnime = async (name: string): Promise<string | undefined> => 
 
   // Find exact match
   const anime = animes.find(anime => anime.titles.find(title => title.title === name));
-  return anime?.url;
+  if (!anime) return 
+
+  // Add to cache
+  cache.add(name, anime.url);
+
+  // Return URL
+  return anime.url;
 };
