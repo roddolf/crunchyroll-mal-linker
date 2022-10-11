3d7a1f1bbe1408c6221266f1487fa6de86f274dd
Rodolfo Aguirre <aguirreg.rodolfo@gmail.com>
2022-10-10 21:20:28 -0700

    fix: support for non-beta page (#13)

--- script.meta.js
+++ script.meta.js
@@ -4,8 +4,8 @@
 // @version      1.4.0
 // @description  Add links into Crunchyroll for MAL anime library.
 // @author       Rodolfo Aguirre
-// @include      /^https?:\/\/www.crunchyroll.com\/([a-z-]+/)?[a-zA-Z-\d]+$/
-// @include      /^https?:\/\/beta.crunchyroll.com/
+// @match        https://www.crunchyroll.com/*
+// @match        https://beta.crunchyroll.com/*
 // @updateURL    https://raw.githubusercontent.com/roddolf/crunchyroll-mal-linker/master/script.meta.js
 // @downloadURL  https://raw.githubusercontent.com/roddolf/crunchyroll-mal-linker/master/dist/script.user.js
 // @connect      api.jikan.moe
